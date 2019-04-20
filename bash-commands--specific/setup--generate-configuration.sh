a0bbeacb8f4eb1b34be97546a8ffab0c8ef640f7
David Lechner <david.lechner@talsen.team>
2019-04-20 11:00:49 +0200

    fix vs code setup script

--- bash-commands--specific/setup--generate-configuration.sh
+++ bash-commands--specific/setup--generate-configuration.sh
@@ -11,4 +11,7 @@ echo -E "Creating and starting containers and networks ..."
 export HOST_SERVICE_SERVER_TAIGA_IO_COMMAND=generate
 export HOST_SERVICE_SERVER_TAIGA_IO_RESTART=no
 
+docker-compose --file ${HOST_PATH_TO_DOCKER_COMPOSE_FILE} \
+               up
+
 echo -e "Creating and starting containers and networks ... $( __done )"
