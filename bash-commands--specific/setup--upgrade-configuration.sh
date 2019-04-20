72d7150da12b8134779a61471cf2abaa76f3f812
David Lechner <david.lechner@talsen.team>
2019-04-20 19:33:03 +0200

    add specific setup task upgrade configuration

--- bash-commands--specific/setup--upgrade-configuration.sh
+++ bash-commands--specific/setup--upgrade-configuration.sh
@@ -6,12 +6,12 @@ source bash-util/functions.sh
 
 prepare_local_environment ${@}
 
-echo -E "Generating taiga-io configuration ..."
+echo -E "Upgrading taiga-io configuration ..."
 
-export HOST_SERVICE_SERVER_TAIGA_IO_COMMAND=generate
+export HOST_SERVICE_SERVER_TAIGA_IO_COMMAND=upgrade
 export HOST_SERVICE_SERVER_TAIGA_IO_RESTART=no
 
 docker-compose --file ${HOST_PATH_TO_DOCKER_COMPOSE_FILE} \
                up
 
-echo -e "Generating taiga-io configuration ... $( __done )"
+echo -e "Upgrading taiga-io configuration ... $( __done )"
