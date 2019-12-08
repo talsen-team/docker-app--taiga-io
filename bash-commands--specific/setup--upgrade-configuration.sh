d04f0abba89a808fa348407ddf4421fafe350e89
David Lechner <david.lechner@talsen.team>
2019-12-08 14:35:15 +0100

    add compatibility flag to support memory limits at container creation

--- bash-commands--specific/setup--upgrade-configuration.sh
+++ bash-commands--specific/setup--upgrade-configuration.sh
@@ -11,7 +11,7 @@ echo -E "Upgrading taiga-io configuration ..."
 export HOST_SERVICE_SERVER_TAIGA_IO_COMMAND=upgrade
 export HOST_SERVICE_SERVER_TAIGA_IO_RESTART=no
 
-docker-compose --file ${HOST_PATH_TO_DOCKER_COMPOSE_FILE} \
+docker-compose --compatibility --file ${HOST_PATH_TO_DOCKER_COMPOSE_FILE} \
                up
 
 echo -e "Upgrading taiga-io configuration ... $( __done )"
